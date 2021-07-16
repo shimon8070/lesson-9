ca7ce85c00419253fb5855ed2661dfc9dbe3ddbf
Shimon <shimon8090@gmail.com>
2021-07-16 22:33:43 +0300

    added signup component

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,14 +2,38 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+const config =  {
+  apiKey: "AIzaSyDu7aZbSfIrN6br5V9XMRSYtZbqhdzt0iY",
+  authDomain: "crwn-ecom-8fb4f.firebaseapp.com",
+  projectId: "crwn-ecom-8fb4f",
+  storageBucket: "crwn-ecom-8fb4f.appspot.com",
+  messagingSenderId: "636299632712",
+  appId: "1:636299632712:web:89ceac1a93dd16d7fcf87d"
+};
+
+export const createUserProfileDocument = async (userAuth,additionalData)=>{
+  if (!userAuth) return;
+
+  const userRef =firestore.doc(`users/${userAuth.uid}`)
+
+  const snapShot = await userRef.get()
+
+  if(!snapShot.exists){
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try{
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    }catch(error){
+      console.log("error creating user", error.message);
+    }
+  }
+  return userRef;
 };
 
 firebase.initializeApp(config);
