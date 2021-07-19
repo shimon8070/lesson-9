07138dcdd39ebbe40e7d55d6aafd3f03f300dfad
Shimon <shimon8090@gmail.com>
2021-07-19 17:43:53 +0300

    Cart dropdown / icon

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import {connect} from 'react-redux';
 import { auth } from '../../firebase/firebase.utils';
+import {createStructuredSelector} from "reselect";
 import CartIcon from "../cart-icon/cart-icon.component";
-import { ReactComponent as Logo } from '../../assets/crown.svg';
 import CartDropdown from "../cart-dropdown/cart-dropdown.component";
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+import {selectCartHidden} from "../../redux/cart/cart.selectors";
+import {selectCurrentUser} from "../../redux/cart/user.selector";
 import './header.styles.scss';
 
 const Header = ({ currentUser, hidden }) => (
@@ -35,8 +38,8 @@ const Header = ({ currentUser, hidden }) => (
       }
   </div>
 );
-const mapStateToProps = ({user:{currentUser}, cart:{hidden}}) => ({
-    currentUser,
-    hidden
+const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser,
+    hidden: selectCartHidden
 })
 export default connect(mapStateToProps)(Header);
